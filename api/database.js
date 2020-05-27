134627727107f441f9c73774e13614b5197879ad
Maxim Baduk <mb2474@cornell.edu>
2020-05-27 01:25:18 -0400

    clean up database initialization process

--- api/database.js
+++ api/database.js
@@ -9,6 +9,11 @@ const https = require('https')
 const DBSOURCE = "db.sqlite"
 const rosterHost = "https://classes.cornell.edu"
 
+
+/*
+    Opens the connection to our database. Creates the db file if one does not
+    already exist, and generates empty tables. 
+*/
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
       // Cannot open database
@@ -17,39 +22,35 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     } else {
         console.log('Connected to the SQLite database.')
         db.serialize(() => {
-            db.run(`CREATE TABLE class (
-                class_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name text, 
-                course_number INTEGER,
-                req_id INTEGER
-                )`,
-                (err) => {
-                    if (err) {
-                        // Table already created
-                    } else {
-                        // Table just created, creating some rows
-                        var insert = 'INSERT INTO class (name, course_number, req_id) VALUES (?, ?, ?)'
-                        db.run(insert, ["CS_2110", "12345", 1])
-                        db.run(insert, ["ECE_2720", "33333", 1])
-                    }
-                }
-            ).run(`CREATE TABLE req (
-                req_id INTEGER PRIMARY_KEY AUTOINCREMENT,
-                code text
-                )`,
-                (err) => {
-                    if (err) {
-                        // Table already created
-                    } else {
-                        // Table just created, creating some rows
-                        var sql_req = 'INSERT INTO req (code) VALUES (?,)'
-                        db.run(sql_req, ["HA-AS"])
-                        db.run(sql_req, ["MQR-AS"])
-                    }
-                })
+            db.run(createTableClassSQL, err => err ? console.log(err) : null)
+              .run(createTableReqSQL, err => err ? console.log(err) : null)
+              .run(createTableClassReqSQL, err => err ? console.log(err) : null)
         });
     }
 });
+
+// SQL: Creates a `class` table. 
+const createTableClassSQL = 
+    `CREATE TABLE IF NOT EXISTS class (
+        course_id INTEGER PRIMARY KEY NOT NULL, 
+        name TEXT NOT NULL
+    )` 
+
+// SQL: Creates a `req` table.
+const createTableReqSQL = 
+    `CREATE TABLE IF NOT EXISTS req (
+        req_id INTEGER PRIMARY KEY NOT NULL, 
+        code TEXT NOT NULL
+    )` 
+
+// SQL: Creates a `class_req` table.
+const createTableClassReqSQL = 
+    `CREATE TABLE IF NOT EXISTS class_req (
+        course_id INTEGER NOT NULL, 
+        req_id INTEGER NOT NULL, 
+        PRIMARY KEY (course_id, req_id)
+    )` 
+
 
 /* Example of populating all FA14 Math classes to our db. */
 const refetch = () => {
@@ -63,14 +64,14 @@ const refetch = () => {
             resJson = JSON.parse(data);
             //console.log(resJson.data.classes[0]);
             for (var i = 0; i < 3; i++) { // Only input 3 classes as example
-                inputClass(resJson.data.classes[i]);
+                inputClass(resJson.data.classes[i], []);
             }
         })
         
     }).on("error", (err) => console.error(err));
 }
 
-const inputClass = (cls) => {
+const inputClass = (cls, reqs) => {
     var sql = `INSERT INTO class (
                 name, course_number, req_id
                ) VALUES (
